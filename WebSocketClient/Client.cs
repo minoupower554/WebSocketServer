93a038b6e90e6f051a989f34917d0921539b4aa6
minoupower554 <ryan.heinschke2010@gmail.com>
2024-12-02 14:27:02 +0000

    started with Standards and also the Client Upload System

--- WebSocketClient/Client.cs
+++ WebSocketClient/Client.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using WebSocketStandards;
+using System.Net.WebSockets;
+using System.Text.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
+namespace WebSocketClient;
+
+public class Program {
+    public static async Task Main() {
+        Uri URI = new("ws://localhost:8080");
+        ClientWebSocket webSocket = new();
+        await webSocket.ConnectAsync(URI, CancellationToken.None);
+        
+    }
+
+    private static async Task SendMessage(ClientWebSocket webSocket, string fileId, int chunkIndex, int maxChunks, byte[] chunkData) {
+        var header = new {
+            fileId,
+            chunkIndex,
+            maxChunks,
+        };
+
+        var jsonHeader = JsonSerializer.Serialize(header);
+        var byteHeader = Encoding.UTF8.GetBytes(jsonHeader);
+
+        
+    }
+}
---
+++ WebSocketStandards/Standards.cs
@@ -0,0 +1,6 @@
+﻿namespace WebSocketStandards;
+
+public static class Standards {
+    private static ushort _frameSize = 65535;
+    public static ushort FrameSize { get => _frameSize; }
+}
