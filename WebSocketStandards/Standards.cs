eb9a13f30095b6bdf3a94c69505d38eee9faaf31
Ryan Heinschke <ryan.heinschke2010@gmail.com>
2024-12-03 20:57:37 +0100

    finished PrepareMessage.

--- WebSocketStandards/Standards.cs
+++ WebSocketStandards/Standards.cs
@@ -2,6 +2,6 @@
 
 public static class Standards
 {
-  public const uint FrameSize = 524288; // in bytes
-  public static readonly string Delimiter = "\r\n\r\n";
+  public const int FrameSize = 524288; // in bytes
+  public const string Delimiter = "\r\n\r\n";
 }
