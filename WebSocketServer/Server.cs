1757a9652b57c21cffd2026d885acc1b0e12ec6f
minoupower554 <ryan.heinschke2010@gmail.com>
2024-12-09 12:42:49 +0000

    began with Server

--- WebSocketServer/Server.cs
+++ WebSocketServer/Server.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WebSocketStandards;
+using System.Net.WebSockets;
+using System.Text.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace WebSockerServer;
+
+class Program {
+    private static async Task Main() {}
+}
