06da2d09da6dcd23028d6662bba9aa756d9f6807
minoupower554 <ryan.heinschke2010@gmail.com>
2024-12-11 16:52:09 +0000

    started with server

--- WebSocketServer/Server.cs
+++ WebSocketServer/Server.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using WebSocketStandards;
+using System.Net;
 using System.Net.WebSockets;
 using System.Text.Json;
 using System.Text;
@@ -10,5 +11,18 @@ using System.Threading.Tasks;
 namespace WebSockerServer;
 
 class Program {
-    private static async Task Main() {}
+    private static async Task Main() {
+        HttpListener listener = new();
+        listener.Prefixes.Add("http://localhost:8080/");
+        listener.Start();
+        HttpListenerContext httpContext =  await listener.GetContextAsync();
+        if (httpContext.Request.IsWebSocketRequest) {
+            HttpListenerWebSocketContext context = await httpContext.AcceptWebSocketAsync(null);
+        } else {
+            httpContext.Response.StatusCode = 426;
+            httpContext.Response.ContentEncoding = Encoding.ASCII;
+            httpContext.Response.Headers.Add(HttpRequestHeader.Upgrade, "websocket");
+            httpContext.Response.Headers.Add()
+        }
+    }
 }
